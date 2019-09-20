bb5a97815186115846b2402701e3a58304cad24e
RakeshBatra <raakesh.batra@rediffmail.com>
2019-09-20 15:42:14 +0000

    vendor: More Addition to Colt
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

---
+++ build/core/main.mk
@@ -0,0 +1,2 @@
+# Include ColtOS versions
+include $(TOPDIR)vendor/colt/build/core/main_version.mk
---
+++ build/core/main_version.mk
@@ -0,0 +1,7 @@
+# COLTOS System Version
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.colt.build.date=$(COLT_BUILD_DATE) \
+    ro.colt.build.version=$(COLT_BUILD_VERSION) \
+    ro.colt.fingerprint=$(COLT_FINGERPRINT) \
+    ro.colt.releasetype=$(COLT_BUILD_TYPE) \
+    ro.colt.version=$(COLT_VERSION)
