a9290f00cb73ac01e6cab71147b1e0c7cfee03bd
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-14 07:07:09 +0200

    vendor: Port face unlock feature [2/4]
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -126,6 +126,16 @@ PRODUCT_PACKAGES += \
     ThemePicker \
     WallpaperPicker2
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # OTA Support
 ifeq ($(COLT_BUILD_TYPE), Official)
 PRODUCT_PACKAGES += \
