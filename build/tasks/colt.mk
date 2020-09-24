a99090231e2217516b6730e370da22268a2f4bda
Bruno Martins <bgcngm@gmail.com>
2020-09-24 11:51:33 +0100

    vendor: Use $(HOST_PREBUILT_TAG) where possible
    Change-Id: If211d23ce72b3404368cd723cbb9ea801e61802c
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- build/tasks/colt.mk
+++ build/tasks/colt.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2017 Unlegacy-Android
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017,2020 The LineageOS Project
 # Copyright (C) 2020 The ColtOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,7 +19,7 @@
 
 COLT_TARGET_PACKAGE := $(PRODUCT_OUT)/$(COLT_VERSION).zip
 
-MD5 := prebuilts/build-tools/path/$(HOST_OS)-x86/md5sum
+MD5 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/md5sum
 
 .PHONY: colt
 colt: $(INTERNAL_OTA_PACKAGE_TARGET)
