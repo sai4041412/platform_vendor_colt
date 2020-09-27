6f71202659a3984d823bea1d9ced02ca5680bb4a
nitin1438 <nitin.chobhe@gmail.com>
2020-09-27 22:51:48 +0530

    Revert "qcom_target cleanup"
    This reverts commit cc56edd5e2562a68a58d132678f77e84b3dd37d8.

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -1,3 +1,10 @@
+define wlan-set-path-variant
+$(call project-set-path-variant,wlan,TARGET_WLAN_VARIANT,hardware/$(1)/wlan)
+endef
+define bt-vendor-set-path-variant
+$(call project-set-path-variant,bt-vendor,TARGET_BT_VENDOR_VARIANT,hardware/$(1)/bt)
+endef
+
 # Set device-specific HALs into project pathmap
 define set-device-specific-path
 $(if $(USE_DEVICE_SPECIFIC_$(1)), \
@@ -10,6 +17,8 @@ endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
+$(call set-device-specific-path,QCOM_SUPERPROJECT,qcom_superproject,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT))
+
 $(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
 $(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
 $(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
@@ -20,9 +29,26 @@ $(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource
 $(call set-device-specific-path,POWER,power,hardware/qcom-caf/power)
 $(call set-device-specific-path,THERMAL,thermal,hardware/qcom-caf/thermal)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
-$(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
+
+$(call wlan-set-path-variant,qcom-caf)
+$(call bt-vendor-set-path-variant,qcom-caf)
 
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+else
+
+$(call project-set-path,qcom-audio,hardware/qcom/audio/default)
+$(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFORM))
+$(call project-set-path,qcom-media,hardware/qcom/media/$(TARGET_BOARD_PLATFORM))
+
+$(call project-set-path,qcom-camera,hardware/qcom/camera)
+$(call project-set-path,qcom-data-ipa-cfg-mgr,hardware/qcom/data/ipacfg-mgr)
+$(call project-set-path,qcom-gps,hardware/qcom/gps)
+$(call project-set-path,qcom-sensors,hardware/qcom/sensors)
+$(call project-set-path,qcom-loc-api,vendor/qcom/opensource/location)
+$(call project-set-path,qcom-dataservices,$(TARGET_DEVICE_DIR)/dataservices)
+
+$(call wlan-set-path-variant,qcom)
+$(call bt-vendor-set-path-variant,qcom)
 
 endif
