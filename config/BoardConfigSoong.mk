PixelExperience/vendor_aosp

vendor: Allow forcing custom build fingerprint [1/2]
Change-Id: I783a19829cb9d655f8abaeafa14f1f3d6a12ed20

config/BoardConfigSoong.mk
@@ -36,6 +36,10 @@ SOONG_CONFIG_customGlobalVars += \
     target_surfaceflinger_fod_lib \
     uses_camera_parameter_lib
 
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_customGlobalVars += force_build_fingerprint
+endif
+
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
 SOONG_CONFIG_customNvidiaVars += \
     uses_nv_enhancements
@@ -84,6 +88,11 @@ SOONG_CONFIG_customGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET
 SOONG_CONFIG_customGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_customGlobalVars_target_surfaceflinger_fod_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
 SOONG_CONFIG_customGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
+
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_customGlobalVars_force_build_fingerprint := $(TARGET_FORCE_BUILD_FINGERPRINT)
+endif
+
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_customQcomVars_qcom_soong_namespace := $(QCOM_SOONG_NAMESPACE)
 endif

sai4041412/platform_vendor_colt

config/BoardConfigSoong.mk
@@ -41,6 +41,11 @@ SOONG_CONFIG_coltGlobalVars += \
     target_surfaceflinger_fod_lib \
     uses_camera_parameter_lib
 
+
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_coltGlobalVars += force_build_fingerprint
+endif
+
 SOONG_CONFIG_NAMESPACES += coltNvidiaVars
 SOONG_CONFIG_coltNvidiaVars += \
     uses_nv_enhancements
@@ -99,6 +104,11 @@ ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_coltQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
 else
 SOONG_CONFIG_coltQcomVars_qcom_display_headers_namespace := $(QCOM_SOONG_NAMESPACE)/display
+
+ifneq ($(TARGET_FORCE_BUILD_FINGERPRINT),)
+SOONG_CONFIG_coltGlobalVars_force_build_fingerprint := $(TARGET_FORCE_BUILD_FINGERPRINT)
+endif
+
 endif
 
 ifneq ($(TARGET_USE_QTI_BT_STACK),true)